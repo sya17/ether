f1e52ed41c92bd2fd28195420a88f5b22ebeb108
Sarip Hidayatullah <102409123+sya17@users.noreply.github.com>
2024-02-28 15:54:56 +0700

    fix:

--- src/components/page/test-page.tsx
+++ src/components/page/test-page.tsx
@@ -1,21 +1,21 @@
 "use client";
 
-import { useState } from "react";
-import ButtonAdd from "../common/table/button-add";
+// import { useState } from "react";
+// import ButtonAdd from "../common/table/button-add";
 
 export default function TestPage({}) {
-  console.log("TESTTT ");
-  const [data, setData] = useState(false);
-  console.log("data ", data);
-  const [openDetail, setOpenDetail] = useState<{ open: boolean; data?: any }>({
-    open: false,
-    data: undefined,
-  });
+  // console.log("TESTTT ");
+  // const [data, setData] = useState(false);
+  // console.log("data ", data);
+  // const [openDetail, setOpenDetail] = useState<{ open: boolean; data?: any }>({
+  //   open: false,
+  //   data: undefined,
+  // });
 
   return (
     <div>
-      <ButtonAdd openAdd={setOpenDetail} />
-      <div>TEST</div>
+      {/* <ButtonAdd openAdd={setOpenDetail} />
+      <div>TEST</div> */}
     </div>
   );
 }
